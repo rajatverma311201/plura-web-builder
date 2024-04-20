d15c834ca9f5cbdde18b3a6927c7986193d25edb
Rajat Verma <rajat311201@gmail.com>
2024-04-21 02:59:54 +0530

    ✅ prisma schema setup

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,6 +1,15 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 interface AgencyDashboardProps {}
 
-const AgencyDashboard: React.FC<AgencyDashboardProps> = ({}) => {
+const AgencyDashboard: React.FC<AgencyDashboardProps> = async ({}) => {
+    const authUser = await currentUser();
+
+    if (!authUser) {
+        return redirect("agency/sign-in");
+    }
+
     return <h1>AgencyDashboard</h1>;
 };
 export default AgencyDashboard;
